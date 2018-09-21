1de23e7038ee79b016361d4350596f789eb87471
Eric Garside <garside@gmail.com>
2018-09-21 08:09:45 -0400

    Stubbing in base loggers

---
+++ technologic/lib/technologic/loggers/base.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Technologic
+  module Loggers
+    class Base
+      # TODO: Copy over ApplicationLogger
+    end
+  end
+end
---
+++ technologic/spec/loggers/base_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Technologic::Loggers::Base do
+  it "needs specs"
+end
