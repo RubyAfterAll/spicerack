222cc1c28cce9116b38ad76ef9acf4ae67e3bcad
Allen Rettberg <allen.rettberg@freshly.com>
2018-11-05 11:26:31 -0500

    Backport changes from freshly codebse

--- rspice/lib/rspice/shared_examples/a_class_pass_method.rb
+++ rspice/lib/rspice/shared_examples/a_class_pass_method.rb
@@ -1,36 +1,44 @@
 # frozen_string_literal: true
 
-# TODO: Jank - this should use Method#parameters, not Method#arity
-
 RSpec.shared_examples_for "a class pass method" do |method|
   subject do
-    if takes_options?
-      test_class.public_send(method, *arguments, **options)
+    if accepts_block?
+      test_class.public_send(method, *arguments, &block)
     else
       test_class.public_send(method, *arguments)
     end
   end
 
   let(:test_class) { described_class }
-  let(:initialize_arity) { test_class.instance_method(:initialize).arity }
-  let(:takes_options?) { (initialize_arity < 0) }
-  let(:argument_arity) { takes_options? ? (initialize_arity.abs - 1) : initialize_arity }
+
+  let(:method_parameters) { test_class.instance_method(:initialize).parameters }
+  let(:argument_arity) { method_parameters.map(&:first).select { |type| type.in?(%i[req opt]) }.length }
+  let(:option_keys) { method_parameters.select { |param| param.first.in?(%i[key keyreq]) }.map(&:last) }
+  let(:accepts_block?) { method_parameters.any? { |param| param.first == :block } }
 
   let(:arguments) do
-    Array.new(argument_arity) { double }
+    Array.new(argument_arity) { double }.tap do |array|
+      array << options if options.present?
+    end
   end
-  let(:options) { takes_options? ? Hash[*Faker::Lorem.words(2 * rand(1..2))].symbolize_keys : nil }
+  let(:options) { option_keys.each_with_object({}) { |key, hash| hash[key] = Faker::Lorem.word } }
+  let(:block) { -> {} }
   let(:instance) { instance_double(test_class) }
   let(:output) { double }
 
   before do
     allow(instance).to receive(method).and_return(output)
-    if takes_options?
-      allow(test_class).to receive(:new).with(*arguments, **options).and_return(instance)
+
+    if accepts_block?
+      allow(test_class).to receive(:new).with(*arguments, &block).and_return(instance)
     else
       allow(test_class).to receive(:new).with(*arguments).and_return(instance)
     end
   end
 
   it { is_expected.to eq output }
+
+  it "has matching parameters with initialize" do
+    expect(test_class.method(method).parameters).to eq test_class.instance_method(:initialize).parameters
+  end
 end
