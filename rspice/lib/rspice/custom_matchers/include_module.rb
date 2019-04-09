a3f6a4647a27bd6277e36e833404b2bcda0c223a
Eric Garside <garside@gmail.com>
2019-04-09 12:12:21 -0400

    RSpice: Fix inherit_from
    Was using the old janky style of matcher.
    
    Syntax cleaned up the other module tree helpers as well.

--- rspice/lib/rspice/custom_matchers/include_module.rb
+++ rspice/lib/rspice/custom_matchers/include_module.rb
@@ -11,21 +11,10 @@
 # end
 
 RSpec::Matchers.define :include_module do |module_class|
-  match do
-    test_subject.included_modules.include? module_class
-  end
-
-  description do
-    "included the module #{module_class}"
-  end
-
-  failure_message do |described_class|
-    "expected #{described_class} to include module #{module_class}"
-  end
-
-  failure_message_when_negated do |described_class|
-    "expected #{described_class} not to include module #{module_class}"
-  end
+  match { test_subject.included_modules.include? module_class }
+  description { "included the module #{module_class}" }
+  failure_message { |described_class| "expected #{described_class} to include module #{module_class}" }
+  failure_message_when_negated { |described_class| "expected #{described_class} not to include module #{module_class}" }
 
   def test_subject
     subject.is_a?(Class) ? subject : subject.class
