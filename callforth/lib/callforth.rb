fd0468bc8bad2ba5700f9a9f7dbf2bca9cbaeb38
Eric Garside <garside@gmail.com>
2019-05-07 00:00:40 -0400

    Callforth: Configuration

--- callforth/lib/callforth.rb
+++ callforth/lib/callforth.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require "active_support"
+require "active_support/callbacks"
+
 require "callforth/version"
+require "callforth/config_options"
+require "callforth/setup"
 
 module Callforth
-  # Your code goes here...
+  mattr_accessor :secret_key
 end
+
+require "callforth/railtie" if defined?(Rails)
