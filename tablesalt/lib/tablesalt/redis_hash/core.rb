28954fc8985b406e931822ea87d65c44b84f608a
Eric Garside <garside@gmail.com>
2019-05-14 18:20:06 -0400

    Tablesalt: RedisHash: Set Default

--- tablesalt/lib/tablesalt/redis_hash/core.rb
+++ tablesalt/lib/tablesalt/redis_hash/core.rb
@@ -11,8 +11,12 @@ module Tablesalt
         delegate :del, :hdel, :hexists, :hget, :hgetall, :hkeys, :hlen, :hmget, :hmset, :hset, :hvals, to: :redis
       end
 
-      def initialize(redis_key = nil, redis: Redis.new)
+      def initialize(redis_key = nil, default: nil, redis: Redis.new, &block)
+        raise ArgumentError, "cannot specify both block and static default" if block_given? && !default.nil?
+
         run_callbacks(:initialize) do
+          self.default_proc = block if block_given?
+          self.default = default if default.present?
           @redis_key = redis_key || SecureRandom.hex
           @redis = redis
         end
--- tablesalt/spec/tablesalt/redis_hash/core_spec.rb
+++ tablesalt/spec/tablesalt/redis_hash/core_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Tablesalt::RedisHash::Core, type: :module do
 
   subject { example_class.new }
 
-  let(:example_class) { example_class_having_callback.include(described_class) }
+  let(:example_class) do
+    example_class_having_callback.tap do |klass|
+      klass.include Tablesalt::RedisHash::Default
+      klass.include described_class
+    end
+  end
 
   it { is_expected.to delegate_method(:del).to(:redis) }
   it { is_expected.to delegate_method(:hdel).to(:redis) }
@@ -70,6 +75,38 @@ RSpec.describe Tablesalt::RedisHash::Core, type: :module do
       it_behaves_like "an instance" do
         let(:expected_redis_key) { redis_key }
         let(:expected_redis) { redis }
+      end
+    end
+
+    context "with only a default" do
+      subject(:instance) { example_class.new(default: :default) }
+
+      it_behaves_like "an instance"
+
+      it "has a default" do
+        expect(instance.default).to eq :default
+      end
+    end
+
+    context "with only a block" do
+      subject(:instance) do
+        example_class.new { :default }
+      end
+
+      it_behaves_like "an instance"
+
+      it "has a default" do
+        expect(instance.default_proc).to be_a Proc
+      end
+    end
+
+    context "with a default and block" do
+      subject(:instance) do
+        example_class.new(default: :default) { :default }
+      end
+
+      it "raises" do
+        expect { instance }.to raise_error ArgumentError, "cannot specify both block and static default"
       end
     end
   end
