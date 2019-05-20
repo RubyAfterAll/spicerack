47557560403d911fb0667c36f38453018235fa0b
Allen Rettberg <allen.rettberg@freshly.com>
2019-05-20 01:54:54 -0400

    fix ArrayIndex#freeze

--- tablesalt/lib/tablesalt/array_index.rb
+++ tablesalt/lib/tablesalt/array_index.rb
@@ -11,13 +11,19 @@ module Tablesalt
     delegate :[], to: :index
 
     def initialize(array)
-      @array = array
+      @array = array.deep_dup
     end
 
     def index
       @index ||= array.each_with_index.each_with_object({}) do |(element, index), hash|
         hash[element] = index unless hash.key?(element)
       end
+    end
+
+    def freeze
+      index.freeze
+      array.freeze
+      super
     end
   end
 end
