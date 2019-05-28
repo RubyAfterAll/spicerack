325724317598f0ec78e276ee599697a7676701a2
Eric Garside <garside@gmail.com>
2019-05-28 10:50:24 -0400

    Ascriptor

--- tablesalt/lib/tablesalt.rb
+++ tablesalt/lib/tablesalt.rb
@@ -7,7 +7,6 @@ require "short_circu_it"
 require "tablesalt/version"
 
 require "tablesalt/dsl/defaults"
-require "tablesalt/dsl/options"
 require "tablesalt/stringable_object"
 
 module Tablesalt; end
