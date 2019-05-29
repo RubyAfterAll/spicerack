0444f9936c188b1bfd243bd4a951aed842f535dc
Eric Garside <garside@gmail.com>
2019-05-29 10:29:16 -0400

    Cleanup Example Input Object

--- spec/support/shared_context/with_an_example_input_object.rb
+++ spec/support/shared_context/with_an_example_input_object.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.shared_context "with an example input object" do |extra_input_object_modules = nil|
+RSpec.shared_context "with an example input object" do
   subject(:example_input_object) { example_input_object_class.new(**input) }
 
-  let(:root_input_object_modules) { [] }
-  let(:input_object_modules) { root_input_object_modules + Array.wrap(extra_input_object_modules) }
-
-  let(:root_input_object_class) { Class.new(Spicerack::InputObject) }
-  let(:example_input_object_class) do
-    root_input_object_class.tap do |input_object_class|
-      input_object_modules.each { |input_object_module| input_object_class.include input_object_module }
-    end
-  end
+  let(:example_input_object_class) { Class.new(Spicerack::InputObject) }
 
   let(:input) do
     {}
