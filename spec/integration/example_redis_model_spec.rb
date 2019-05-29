a06baec8c3d1cd5570f9ea8aacf57534d710fb2c
Eric Garside <garside@gmail.com>
2019-05-28 22:09:32 -0400

    Integration Spec Cleanup

--- spec/integration/example_redis_model_spec.rb
+++ spec/integration/example_redis_model_spec.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-RSpec.describe TaskRedisModel, type: :integration do
+RSpec.describe ExampleRedisModel, type: :integration do
   subject(:redis_model) { described_class.new }
 
   it { is_expected.to inherit_from Spicerack::RedisModel }
   it { is_expected.to include_module Spicerack::HashModel }
+  it { is_expected.to inherit_from ExampleRedisModelParent }
   it { is_expected.to define_field :started_at, :datetime }
   it { is_expected.to define_field :finished_at, :datetime }
+  it { is_expected.to define_field :count, :integer, default: 42 }
+  it { is_expected.to define_field :rate, :float, default: 3.14 }
 
   it { is_expected.to allow_key :started_at }
   it { is_expected.to allow_key :finished_at }
+  it { is_expected.to allow_key :count }
+  it { is_expected.to allow_key :rate }
 
   describe "#data" do
     subject { redis_model.data }
--- spec/support/test_classes/example_redis_model.rb
+++ spec/support/test_classes/example_redis_model.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class TaskRedisModel < Spicerack::RedisModel
+class ExampleRedisModelParent < Spicerack::RedisModel
   field :started_at, :datetime
   field :finished_at, :datetime
 end
 
-class ProcessorRedisModel < TaskRedisModel
+class ExampleRedisModel < ExampleRedisModelParent
   field :count, :integer, default: 42
   field :rate, :float, default: 3.14
 end
