c7cc8c7910dcdb0d2e7095e61d5d71075ff2a218
Allen Rettberg <allen.rettberg@freshly.com>
2019-06-03 13:47:58 -0400

    bump to 0.13.3

--- rspice/lib/rspice/version.rb
+++ rspice/lib/rspice/version.rb
@@ -2,5 +2,5 @@
 
 module Rspice
   # This constant is managed by spicerack
-  VERSION = "0.13.2"
+  VERSION = "0.13.3"
 end
--- short_circu_it/lib/short_circu_it/version.rb
+++ short_circu_it/lib/short_circu_it/version.rb
@@ -2,5 +2,5 @@
 
 module ShortCircuIt
   # This constant is managed by spicerack
-  VERSION = "0.13.2"
+  VERSION = "0.13.3"
 end
