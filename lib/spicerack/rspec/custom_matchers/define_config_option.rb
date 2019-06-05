2d5338abfc47d5a979979e7ae935e3fba16c4376
Allen Rettberg <allen.rettberg@freshly.com>
2019-06-05 13:16:53 -0400

    add details to define_config_option matcher

--- lib/spicerack/rspec/custom_matchers/define_config_option.rb
+++ lib/spicerack/rspec/custom_matchers/define_config_option.rb
@@ -10,16 +10,23 @@
 #     end
 #
 #     RSpec.describe ExampleConfiguration, type: :configuration do
-#       subject { described_class.new }\
+#       subject { described_class.new }
 #
 #       it { is_expected.to define_config_option :foo }
 #       it { is_expected.to define_config_option :bar, default: :baz }
 #     end
 
 RSpec::Matchers.define :define_config_option do |option, default: nil|
+  description { "define config option #{option}" }
+  failure_message { "expected #{subject} to define config option #{option} #{with_default(default)}".strip }
+
   match do |obj|
     expect(obj).to respond_to :config
     expect(obj.config.instance_variable_get(:@config)).to be_present
     expect(obj.config.instance_variable_get(:@config)).to define_option option, default: default
+  end
+
+  def with_default(default)
+    "with default #{default}" unless default.nil?
   end
 end
