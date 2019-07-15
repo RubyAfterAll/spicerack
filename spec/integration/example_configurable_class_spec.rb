39f3294b4f86a90e435534a5d54232af01b08efd
Allen Rettberg <arettberg@gmail.com>
2019-07-15 00:23:01 -0400

    support deeply nested configs

--- spec/integration/example_configurable_class_spec.rb
+++ spec/integration/example_configurable_class_spec.rb
@@ -16,13 +16,19 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
           option :nested_option_without_default
           option :nested_option_with_default, default: "nested default value"
           option(:nested_option_with_default_block) { "nested default value from block" }
+
+          nested :double_nested do
+            option :double_nested_option_without_default
+            option :double_nested_option_with_default, default: "double nested default value"
+            option(:double_nested_option_with_default_block) { "double nested default value from block" }
+          end
         end
       end
     end
   end
 
-  let(:config) { configurable_module.config }
   let(:described_class) { configurable_module }
+  let(:config) { configurable_module.config }
 
   it { is_expected.to define_config_option(:option_without_default) }
   it { is_expected.to define_config_option(:option_with_default, default: "default value") }
@@ -32,6 +38,12 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
     it { is_expected.to define_config_option(:nested_option_without_default) }
     it { is_expected.to define_config_option(:nested_option_with_default, default: "nested default value") }
     it { is_expected.to define_config_option(:nested_option_with_default_block, default: "nested default value from block") }
+
+    nested_config_option :double_nested do
+      it { is_expected.to define_config_option(:double_nested_option_without_default) }
+      it { is_expected.to define_config_option(:double_nested_option_with_default, default: "double nested default value") }
+      it { is_expected.to define_config_option(:double_nested_option_with_default_block, default: "double nested default value from block") }
+    end
   end
 
   it "doesn't define accessors outside of configure block" do
@@ -65,6 +77,10 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
     let(:nested_option_with_default_override) { "nested option with default override" }
     let(:nested_option_with_default_block_override) { "nested option with default block override" }
 
+    let(:double_nested_option_without_default_override) { "double nested option without default override" }
+    let(:double_nested_option_with_default_override) { "double nested option with default override" }
+    let(:double_nested_option_with_default_block_override) { "double nested option with default block override" }
+
     before do
       configurable_module.configure do |konfig|
         konfig.option_without_default = option_without_default_override
@@ -74,6 +90,10 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         konfig.nested_config.nested_option_without_default = nested_option_without_default_override
         konfig.nested_config.nested_option_with_default = nested_option_with_default_override
         konfig.nested_config.nested_option_with_default_block = nested_option_with_default_block_override
+
+        konfig.nested_config.double_nested.double_nested_option_without_default = double_nested_option_without_default_override
+        konfig.nested_config.double_nested.double_nested_option_with_default = double_nested_option_with_default_override
+        konfig.nested_config.double_nested.double_nested_option_with_default_block = double_nested_option_with_default_block_override
       end
     end
 
@@ -82,9 +102,19 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
       expect(configurable_module.config.option_with_default).to eq option_with_default_override
       expect(configurable_module.config.option_with_default_block).to eq option_with_default_block_override
 
-      expect(configurable_module.config.nested_config.nested_option_without_default).to eq nested_option_without_default_override
-      expect(configurable_module.config.nested_config.nested_option_with_default).to eq nested_option_with_default_override
-      expect(configurable_module.config.nested_config.nested_option_with_default_block).to eq nested_option_with_default_block_override
+      expect(configurable_module.config.nested_config.nested_option_without_default).
+        to eq nested_option_without_default_override
+      expect(configurable_module.config.nested_config.nested_option_with_default).
+        to eq nested_option_with_default_override
+      expect(configurable_module.config.nested_config.nested_option_with_default_block).
+        to eq nested_option_with_default_block_override
+
+      expect(configurable_module.config.nested_config.double_nested.double_nested_option_without_default).
+        to eq double_nested_option_without_default_override
+      expect(configurable_module.config.nested_config.double_nested.double_nested_option_with_default).
+        to eq double_nested_option_with_default_override
+      expect(configurable_module.config.nested_config.double_nested.double_nested_option_with_default_block).
+        to eq double_nested_option_with_default_block_override
     end
   end
 end
