713eeea2ee1392071a2e7e6896949d30db9fcc7b
Allen Rettberg <arettberg@gmail.com>
2019-08-22 00:38:03 -0400

    protect from nested name reuse

--- lib/spicerack/configurable/config_object.rb
+++ lib/spicerack/configurable/config_object.rb
@@ -22,22 +22,29 @@ module Spicerack
         private
 
         def option(name, *)
-          raise ArgumentError, "#{name.inspect} is reserved and cannot be used at a config option" if name.to_sym.in? RESERVED_WORDS
+          _ensure_safe_option_name(name)
 
           super
         end
 
         def nested(namespace, &block)
+          _ensure_safe_option_name(namespace)
+
           nested_config_builder_for(namespace).tap do |builder|
             builder.instance_exec(&block)
 
-            _nested_options << namespace
+            _nested_options << namespace.to_sym
             define_method(namespace) { builder.__send__ :configuration }
           end
         end
 
         def nested_config_builder_for(namespace)
           _nested_builders[namespace.to_sym] ||= ConfigBuilder.new
+        end
+
+        def _ensure_safe_option_name(name)
+          raise ArgumentError, "#{name.inspect} is reserved and cannot be used at a config option" if name.to_sym.in? RESERVED_WORDS
+          raise ArgumentError, "#{name.inspect} is already in use" if _nested_options.include?(name.to_sym)
         end
       end
     end
--- spec/spicerack/configurable/config_object_spec.rb
+++ spec/spicerack/configurable/config_object_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Spicerack::Configurable::ConfigObject do
   it { is_expected.to inherit_from Spicerack::InputObject }
   it { is_expected.to include_module Singleton }
 
-  describe ".nested" do
+  describe ".option" do
     let(:name) { Faker::Lorem.word }
     let(:default_value) { Faker::Hipster.sentence }
     let(:config_object_class) do
@@ -36,17 +36,39 @@ RSpec.describe Spicerack::Configurable::ConfigObject do
     let(:nested_option) { Faker::Lorem.word }
     let(:nested_default_value) { Faker::ChuckNorris.fact }
 
-    before do
-      config_object_class.instance_exec(self) do |spec_context|
-        nested spec_context.namespace do
-          option spec_context.nested_option, default: spec_context.nested_default_value
+    let(:nested_config_setup) do
+      lambda do
+        config_object_class.instance_exec(self) do |spec_context|
+          nested spec_context.namespace do
+            option spec_context.nested_option, default: spec_context.nested_default_value
+          end
         end
       end
     end
 
-    it "defines a nested config object" do
-      expect(config.public_send(namespace)).to inherit_from described_class
-      expect(config.public_send(namespace).public_send(nested_option)).to eq nested_default_value
+    context "when an allowed word is used" do
+      before { nested_config_setup.call }
+
+      it "defines a nested config object" do
+        expect(config.public_send(namespace)).to inherit_from described_class
+        expect(config.public_send(namespace).public_send(nested_option)).to eq nested_default_value
+      end
+
+      context "when a nested config is used twice" do
+        it "raises an ArgumentError" do
+          expect(nested_config_setup).
+            to raise_error ArgumentError, "#{namespace.inspect} is already in use"
+        end
+      end
+    end
+
+    context "when a reserved word is used" do
+      let(:namespace) { described_class::RESERVED_WORDS.sample.to_s }
+
+      it "raises an ArgumentError" do
+        expect(nested_config_setup).
+          to raise_error ArgumentError, "\"config_eval\" is reserved and cannot be used at a config option"
+      end
     end
   end
 end
