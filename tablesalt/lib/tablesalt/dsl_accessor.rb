6cd2b3f162d8bcaf8974693b403b548fdb4afca1
Allen Rettberg <arettberg@gmail.com>
2019-08-21 22:22:05 -0400

    define accessors one at a time

--- tablesalt/lib/tablesalt/dsl_accessor.rb
+++ tablesalt/lib/tablesalt/dsl_accessor.rb
@@ -44,24 +44,26 @@ module Tablesalt
       private
 
       def dsl_accessor(*accessors)
-        accessors.each do |attr|
-          define_singleton_method attr do |*args|
-            ivar_name = attr_internal_ivar_name(attr)
+        accessors.each { |attr| _define_dsl_accessor(attr) }
+      end
 
-            if instance_variable_defined?(ivar_name)
-              raise NameError, "internal attribute #{attr} already set" if args.one?
-              raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 0)" if args.size > 1
+      def _define_dsl_accessor(attr)
+        define_singleton_method attr do |*args|
+          ivar_name = attr_internal_ivar_name(attr)
 
-              instance_variable_get(ivar_name)
-            else
-              raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 1)" unless args.one?
+          if instance_variable_defined?(ivar_name)
+            raise NameError, "internal attribute #{attr} already set" if args.one?
+            raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 0)" if args.size > 1
 
-              instance_variable_set(ivar_name, args.first)
-            end
+            instance_variable_get(ivar_name)
+          else
+            raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 1)" unless args.one?
+
+            instance_variable_set(ivar_name, args.first)
           end
-
-          private_class_method attr
         end
+
+        private_class_method attr
       end
     end
   end
