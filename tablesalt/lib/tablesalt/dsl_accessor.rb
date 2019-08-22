555d30aa169304bcf0e665d857ed6c91acdcd0d5
Allen Rettberg <arettberg@gmail.com>
2019-08-21 21:43:19 -0400

    tweak/fix dslacessor log, dump instance method

--- tablesalt/lib/tablesalt/dsl_accessor.rb
+++ tablesalt/lib/tablesalt/dsl_accessor.rb
@@ -5,25 +5,33 @@ require "active_support/core_ext/module/attr_internal"
 module Tablesalt
   module DSLAccessor
     extend ActiveSupport::Concern
+    include Technologic
 
     class_methods do
+      private
+
       def dsl_accessor(*accessors)
         accessors.each do |attr|
           define_singleton_method attr do |*args|
-            raise ArgumentError, "wrong number of arguments (given #{args.size}, expected 1)" if args.size > 1
-
             ivar_name = attr_internal_ivar_name(attr)
+            expected_argument_count = instance_variable_defined?(ivar_name) ? 0 : 1
 
-            instance_variable_set(ivar_name, args.first) if args.size == 1
+            if args.size > expected_argument_count
+              raise NameError, "internal attribute #{attr} already set" if args.one?
 
-            instance_variable_get(ivar_name)
+              raise ArgumentError, "wrong number of arguments (given #{args.size}, expected #{expected_argument_count})"
+            end
+
+            if instance_variable_defined?(ivar_name)
+              instance_variable_get(ivar_name)
+            else
+              raise ArgumentError, "wrong number of arguments (given #{args.size}, expected #{expected_argument_count})" if args.none?
+
+              instance_variable_set(ivar_name, args.first) if args.size
+            end
           end
 
           private_class_method attr
-
-          define_method attr do
-            instance_variable_get attr_internal_ivar_name(attr)
-          end
         end
       end
     end
