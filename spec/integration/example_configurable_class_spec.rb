81011a494e7fe36231fd65685568efef49c33c68
Allen Rettberg <arettberg@gmail.com>
2019-09-30 23:35:07 -0400

    assign attributes in integration test

--- spec/integration/example_configurable_class_spec.rb
+++ spec/integration/example_configurable_class_spec.rb
@@ -12,10 +12,18 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         option :option_with_default, default: "default value"
         option(:option_with_default_block) { "default value from block" }
 
+        option :option_without_default_to_assign
+        option :option_with_default_to_assign, default: "default value to assign"
+        option(:option_with_default_block_to_assign) { "default value from block to assign" }
+
         nested :nested_config do
           option :nested_option_without_default
           option :nested_option_with_default, default: "nested default value"
           option(:nested_option_with_default_block) { "nested default value from block" }
+
+          option :nested_option_without_default_to_assign
+          option :nested_option_with_default_to_assign, default: "nested default value to assign"
+          option(:nested_option_with_default_block_to_assign) { "nested default value from block to assign" }
 
           nested :double_nested do
             option :double_nested_option_without_default
@@ -34,10 +42,18 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
   it { is_expected.to define_config_option(:option_with_default, default: "default value") }
   it { is_expected.to define_config_option(:option_with_default_block, default: "default value from block") }
 
+  it { is_expected.to define_config_option(:option_without_default_to_assign) }
+  it { is_expected.to define_config_option(:option_with_default_to_assign, default: "default value to assign") }
+  it { is_expected.to define_config_option(:option_with_default_block_to_assign, default: "default value from block to assign") }
+
   nested_config_option :nested_config do
     it { is_expected.to define_config_option(:nested_option_without_default) }
     it { is_expected.to define_config_option(:nested_option_with_default, default: "nested default value") }
     it { is_expected.to define_config_option(:nested_option_with_default_block, default: "nested default value from block") }
+
+    it { is_expected.to define_config_option(:nested_option_without_default_to_assign) }
+    it { is_expected.to define_config_option(:nested_option_with_default_to_assign, default: "nested default value to assign") }
+    it { is_expected.to define_config_option(:nested_option_with_default_block_to_assign, default: "nested default value from block to assign") }
 
     nested_config_option :double_nested do
       it { is_expected.to define_config_option(:double_nested_option_without_default) }
@@ -53,6 +69,11 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         expect { config.option_with_default = double }.to raise_error NoMethodError
         expect { config.option_with_default_block = double }.to raise_error NoMethodError
       end
+
+      it "doesn't define attribute assignment methods" do
+        expect { config.assign }.to raise_error NoMethodError
+        expect { config.assign_attributes }.to raise_error NoMethodError
+      end
     end
 
     describe "nested config vars" do
@@ -60,6 +81,11 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         expect { config.nested_config.nested_option_without_default = double }.to raise_error NoMethodError
         expect { config.nested_config.nested_option_with_default = double }.to raise_error NoMethodError
         expect { config.nested_config.nested_option_with_default_block = double }.to raise_error NoMethodError
+      end
+
+      it "doesn't define attribute assignment methods" do
+        expect { config.nested_config.assign }.to raise_error NoMethodError
+        expect { config.nested_config.assign_attributes }.to raise_error NoMethodError
       end
     end
 
@@ -71,6 +97,11 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
           to raise_error NoMethodError
         expect { config.nested_config.double_nested.double_nested_option_with_default_block = double }.
           to raise_error NoMethodError
+      end
+
+      it "doesn't define attribute assignment methods" do
+        expect { config.nested_config.double_nested.assign }.to raise_error NoMethodError
+        expect { config.nested_config.double_nested.assign_attributes }.to raise_error NoMethodError
       end
     end
   end
@@ -136,6 +167,14 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
     let(:double_nested_option_with_default_override) { "double nested option with default override" }
     let(:double_nested_option_with_default_block_override) { "double nested option with default block override" }
 
+    let(:option_without_default_override_to_assign) { "option without default override on assign" }
+    let(:option_with_default_override_to_assign) { "option with default override on assign" }
+    let(:option_with_default_block_override_to_assign) { "option with default block override on assign" }
+
+    let(:nested_option_without_default_override_to_assign) { "nested option without default override on assign" }
+    let(:nested_option_with_default_override_to_assign) { "nested option with default override on assign" }
+    let(:nested_option_with_default_block_override_to_assign) { "nested option with default block override on assign" }
+
     before do
       configurable_module.configure do |konfig|
         konfig.option_without_default = option_without_default_override
@@ -149,6 +188,18 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         konfig.nested_config.double_nested.double_nested_option_without_default = double_nested_option_without_default_override
         konfig.nested_config.double_nested.double_nested_option_with_default = double_nested_option_with_default_override
         konfig.nested_config.double_nested.double_nested_option_with_default_block = double_nested_option_with_default_block_override
+
+        konfig.assign(
+          option_without_default_to_assign: option_without_default_override_to_assign,
+          option_with_default_to_assign: option_with_default_override_to_assign,
+          option_with_default_block_to_assign: option_with_default_block_override_to_assign,
+        )
+
+        konfig.nested_config.assign(
+          nested_option_without_default_to_assign: nested_option_without_default_override_to_assign,
+          nested_option_with_default_to_assign: nested_option_with_default_override_to_assign,
+          nested_option_with_default_block_to_assign: nested_option_with_default_block_override_to_assign,
+        )
       end
     end
 
@@ -157,6 +208,12 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
         expect(configurable_module.config.option_without_default).to eq option_without_default_override
         expect(configurable_module.config.option_with_default).to eq option_with_default_override
         expect(configurable_module.config.option_with_default_block).to eq option_with_default_block_override
+      end
+
+      it "sets the override values with assign" do
+        expect(configurable_module.config.option_without_default_to_assign).to eq option_without_default_override_to_assign
+        expect(configurable_module.config.option_with_default_to_assign).to eq option_with_default_override_to_assign
+        expect(configurable_module.config.option_with_default_block_to_assign).to eq option_with_default_block_override_to_assign
       end
 
       it "is accessible via evaluators" do
@@ -174,6 +231,15 @@ RSpec.describe ExampleConfigurableClass, type: :configuration do
           to eq nested_option_with_default_override
         expect(configurable_module.config.nested_config.nested_option_with_default_block).
           to eq nested_option_with_default_block_override
+      end
+
+      it "sets the override values with assign" do
+        expect(configurable_module.config.nested_config.nested_option_without_default_to_assign).
+          to eq nested_option_without_default_override_to_assign
+        expect(configurable_module.config.nested_config.nested_option_with_default_to_assign).
+          to eq nested_option_with_default_override_to_assign
+        expect(configurable_module.config.nested_config.nested_option_with_default_block_to_assign).
+          to eq nested_option_with_default_block_override_to_assign
       end
 
       it "is accessible via evaluators" do
