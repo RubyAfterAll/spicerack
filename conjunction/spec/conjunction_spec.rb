b8c847285285fe1d2c1e5ddb3a5e20d75cbec49a
Eric Garside <garside@gmail.com>
2019-11-04 13:24:44 -0500

    Conjunction
    What's your function? Encapsulatin' complexity and thinnin' out objects.

--- conjunction/spec/conjunction_spec.rb
+++ conjunction/spec/conjunction_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.describe Spicerack::Conjunction do
+RSpec.describe Conjunction do
   it_behaves_like "a versioned spicerack gem"
 end
--- facet/spec/facet_spec.rb
+++ facet/spec/facet_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.describe Spicerack::Facet do
+RSpec.describe Facet do
   it_behaves_like "a versioned spicerack gem"
 end
