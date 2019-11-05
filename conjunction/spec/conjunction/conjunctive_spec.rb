755c455138d62911f546cbf1774c17ea009fbfd4
Eric Garside <garside@gmail.com>
2019-11-05 01:36:39 -0500

    Conjunction/Junction
    - Fixing bug

--- conjunction/spec/conjunction/conjunctive_spec.rb
+++ conjunction/spec/conjunction/conjunctive_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe Conjunction::Conjunctive, type: :conjunctive do
 
     shared_examples_for "the expected conjunction is returned" do
       context "when respond_to?" do
-        before { allow(junction).to receive(method_name).with(example_conjunctive_name).and_return(conjunction) }
+        before { allow(junction).to receive(method_name).with(example_conjunctive_class).and_return(conjunction) }
 
         it { is_expected.to eq conjunction }
       end
