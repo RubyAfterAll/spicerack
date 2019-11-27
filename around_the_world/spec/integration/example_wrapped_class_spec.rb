872b7e7becff7c9dec585ca5f9c2403828fce803
Allen Rettberg <arettberg@gmail.com>
2019-11-27 13:02:32 -0800

    move things into shared example

--- around_the_world/spec/integration/example_wrapped_class_spec.rb
+++ around_the_world/spec/integration/example_wrapped_class_spec.rb
@@ -6,50 +6,51 @@ RSpec.describe AroundTheWorld do
   subject { receiver }
 
   let(:receiver) { example_wrapped_class.new }
-  let(:around_method_receiver) { receiver.is_a?(Module) ? receiver.singleton_class : receiver.class }
-
   let(:example_wrapped_class) { Class.new.tap { |klass| klass.include(described_class) } }
-  let(:wrapped_method_name) { Faker::Lorem.sentence.parameterize.underscore }
-  let(:return_value) { double }
-
-  let(:primary_args) { Faker::Hipster.words }
-  let(:secondary_args) { Faker::Lorem.words }
-
-  let(:define_wrapped_method_proc) do
-    lambda do |spec_context|
-      define_method(spec_context.wrapped_method_name) do |*args|
-        ExampleCalledClass.primary(*args)
-
-        spec_context.return_value
-      end
-    end
-  end
-
-  let(:wrapper_method_proc) do
-    lambda do |spec_context|
-      around_method spec_context.wrapped_method_name do |*args|
-        ExampleCalledClass.secondary(*spec_context.secondary_args)
-
-        super(*args)
-      end
-    end
-  end
-
-  let(:wrap_method!) { around_method_receiver.instance_exec(self, &wrapper_method_proc) }
-  let(:wrap_method?) { false }
-
-  before do
-    around_method_receiver.instance_exec(self, &define_wrapped_method_proc)
-
-    wrap_method! if wrap_method?
-
-    allow(ExampleCalledClass).to receive(:primary)
-    allow(ExampleCalledClass).to receive(:secondary)
-  end
 
   it { is_expected.to include_module described_class }
 
   shared_examples_for "a wrapped method" do
+    let(:around_method_receiver) { receiver.is_a?(Module) ? receiver.singleton_class : receiver.class }
+
+    let(:wrapped_method_name) { Faker::Lorem.sentence.parameterize.underscore }
+    let(:return_value) { double }
+
+    let(:primary_args) { Faker::Hipster.words }
+    let(:secondary_args) { Faker::Lorem.words }
+
+    let(:define_wrapped_method_proc) do
+      lambda do |spec_context|
+        define_method(spec_context.wrapped_method_name) do |*args|
+          ExampleCalledClass.primary(*args)
+
+          spec_context.return_value
+        end
+      end
+    end
+
+    let(:wrapper_method_proc) do
+      lambda do |spec_context|
+        around_method spec_context.wrapped_method_name do |*args|
+          ExampleCalledClass.secondary(*spec_context.secondary_args)
+
+          super(*args)
+        end
+      end
+    end
+
+    let(:wrap_method!) { around_method_receiver.instance_exec(self, &wrapper_method_proc) }
+    let(:wrap_method?) { false }
+
+    before do
+      around_method_receiver.instance_exec(self, &define_wrapped_method_proc)
+
+      wrap_method! if wrap_method?
+
+      allow(ExampleCalledClass).to receive(:primary)
+      allow(ExampleCalledClass).to receive(:secondary)
+    end
+
     context "when wrapped method is called" do
       before do
         receiver.public_send(wrapped_method_name, *primary_args)
