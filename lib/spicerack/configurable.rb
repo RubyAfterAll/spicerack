83c96f229f8f1beefc5a9622e57d5f81c2dc174c
Allen Rettberg <arettberg@gmail.com>
2020-01-27 23:39:33 -0500

    bump the version in deprecation notice

--- lib/spicerack/configurable.rb
+++ lib/spicerack/configurable.rb
@@ -9,7 +9,7 @@ module Spicerack
 
     def configuration_options(*args)
       puts "DEPRECATION NOTICE: Spicerack::Configurable (used by #{self}) has been deprecated " \
-           "and will be removed in Spicerack #{Gem::Version.new(VERSION)}. " \
+           "and will be removed in Spicerack #{Gem::Version.new(VERSION).bump}. " \
            "Please use Directive instead."
 
       super(*args)
