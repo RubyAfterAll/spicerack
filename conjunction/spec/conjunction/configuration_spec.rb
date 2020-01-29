b3ee7bc21e5ab9299dc2aeaff2c50444a3f85d42
Allen Rettberg <allen.rettberg@freshly.com>
2020-01-28 23:15:51 -0500

    use directive instead of configurable

--- conjunction/spec/conjunction/configuration_spec.rb
+++ conjunction/spec/conjunction/configuration_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe Conjunction::Configuration, type: :configuration do
   subject { described_class }
 
-  it { is_expected.to extend_module Spicerack::Configurable }
+  it { is_expected.to extend_module Directive }
 
   it { is_expected.to define_config_option(:nexus_use_disables_implicit_lookup, default: false) }
   it { is_expected.to define_config_option(:disable_all_implicit_lookup, default: false) }
