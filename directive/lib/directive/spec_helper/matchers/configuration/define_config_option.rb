ae49562621849d4b557facc0ed8a863e7c79b2ff
Allen Rettberg <allen.rettberg@freshly.com>
2020-05-12 14:13:55 -0400

    add "with default.." message to config option matcher

--- directive/lib/directive/spec_helper/matchers/configuration/define_config_option.rb
+++ directive/lib/directive/spec_helper/matchers/configuration/define_config_option.rb
@@ -21,12 +21,20 @@ module Directive
         #       it { is_expected.to define_config_option :foo }
         #       it { is_expected.to define_config_option :bar, default: :baz }
         #     end
-        define :define_config_option do |option, default: nil|
-          description { "define config option #{option.inspect}" }
-          failure_message { "expected #{@obj} to define config option #{option.inspect} with default #{default.inspect}" }
+        define :define_config_option do |option, **config_options|
+          attr_reader :obj, :option, :default, :default_expected
+
+          description { "define config option #{option.inspect}#{with_default_message}" }
+          failure_message { "expected #{obj} to define config option #{option.inspect}#{with_default_message}" }
 
           match do |obj|
+            config_options.assert_valid_keys(:default)
+
             @obj = obj
+            @options = option
+
+            @default_expected = config_options.key?(:default)
+            @default = config_options[:default]
 
             if obj.is_a? Directive::ConfigObject
               expect(obj).to define_option option.to_sym, default: default
@@ -35,6 +43,12 @@ module Directive
               expect(obj.config.instance_variable_get(:@config)).to be_present
               expect(obj.config.instance_variable_get(:@config)).to define_option option.to_sym, default: default
             end
+          end
+
+          def with_default_message
+            return unless default_expected
+
+            " with default #{default.inspect}"
           end
         end
       end
