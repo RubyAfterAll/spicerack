77665879d45f0240fee12e9bc238b5521517609d
Allen Rettberg <arettberg@gmail.com>
2022-04-08 08:11:11 -0700

    fix error message specs

--- tablesalt/spec/tablesalt/dsl_accessor_spec.rb
+++ tablesalt/spec/tablesalt/dsl_accessor_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe Tablesalt::DSLAccessor do
           let(:args) { [ new_value ] }
 
           it "raises" do
-            expect { dsl }.to raise_error NameError, "internal attribute #{accessor} already set"
+            expect { dsl }.to raise_error NameError, %r{internal attribute #{accessor} already set}
           end
         end
 
